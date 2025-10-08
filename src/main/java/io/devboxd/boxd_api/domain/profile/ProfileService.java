e0ea0d720b77d1527ea6e1db053d288063838e44
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-10-08 10:22:06 -0300

    wip: domain_profile

--- src/main/java/io/devboxd/boxd_api/domain/profile/ProfileService.java
+++ src/main/java/io/devboxd/boxd_api/domain/profile/ProfileService.java
@@ -1,11 +1,15 @@
 package io.devboxd.boxd_api.domain.profile;
 
+import io.devboxd.boxd_api.domain.post.Post;
+
 import java.util.List;
 
 public interface ProfileService {
     List<Profile> getAllFollowers();
 
     List<Profile> getAllFollowing();
+
+    List<Post> getAllPost();
 
     Profile getByUsername(String username, List<Profile> listToGet);
 
