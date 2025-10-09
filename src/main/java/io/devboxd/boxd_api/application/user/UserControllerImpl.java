9ad2e119373467ad2db6e1a4629da2b67029ca86
Thiago Henrique <thiagohp070@gmail.com>
2025-10-08 22:22:13 -0300

    feature: configuracao

--- src/main/java/io/devboxd/boxd_api/application/user/UserControllerImpl.java
+++ src/main/java/io/devboxd/boxd_api/application/user/UserControllerImpl.java
@@ -1,7 +1,20 @@
 package io.devboxd.boxd_api.application.user;
 
+import io.devboxd.boxd_api.application.user.dto.GetUserDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserControllerImpl implements UserController {
+    @Override
+    public ResponseEntity<List<GetUserDTO>> getAllByPrefix(String prefix) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<GetUserDTO> getByUsername(String username) {
+        return null;
+    }
 }
