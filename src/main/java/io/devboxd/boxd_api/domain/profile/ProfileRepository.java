9479e1e575bac2f95079ffad9a4c0e38310c3028
Whesley Felipe <111021322+X4vierWhes@users.noreply.github.com>
2025-10-11 16:17:42 -0300

    wip: domain_profile

--- src/main/java/io/devboxd/boxd_api/domain/profile/ProfileRepository.java
+++ src/main/java/io/devboxd/boxd_api/domain/profile/ProfileRepository.java
@@ -1,9 +1,28 @@
 package io.devboxd.boxd_api.domain.profile;
 
+import io.devboxd.boxd_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
 
+    @Override
+    Optional<Profile> findById(Long id);
+
+    Optional<Profile> findByUser(User user);
+
+    @Query("SELECT p.followers FROM Profile p WHERE p.id = :id")
+    List<Profile> findFollowersById(Long id);
+
+    @Query("SELECT p.following FROM Profile p WHERE p.id = :id")
+    List<Profile> findFollowingById(Long id);
+
+    @Query("DElETE p.following FROM Profile p WHERE p.id = :id")
+    List<Profile> deleteFollowingById(Long id);
+
+    @Query("DElETE p.followers FROM Profile p WHERE p.id = :id")
+    List<Profile> deleteFollowerById(Long id);
 }
--- src/main/java/io/devboxd/boxd_api/domain/profile/ProfileServiceImpl.java
+++ src/main/java/io/devboxd/boxd_api/domain/profile/ProfileServiceImpl.java
@@ -1,4 +1,35 @@
 package io.devboxd.boxd_api.domain.profile;
 
-public class ProfileServiceImpl {
+import java.util.List;
+
+public class ProfileServiceImpl implements ProfileService{
+    @Override
+    public List<Profile> getAllFollowers() {
+        return List.of();
+    }
+
+    @Override
+    public List<Profile> getAllFollowing() {
+        return List.of();
+    }
+
+    @Override
+    public Profile getByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public Profile create(Profile profile) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return false;
+    }
+
+    @Override
+    public Profile update(Long id, Profile profile) {
+        return null;
+    }
 }
